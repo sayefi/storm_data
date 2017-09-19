b09907311481be87f11840e8bff1d3c98b818719
sayef <sayefi@gmail.com>
2017-09-18 23:46:56 -0400

    initial analysis

--- rough_script.r
+++ rough_script.r
@@ -1,3 +1,6 @@
+
+## reading input file
+
 inputURL<-"https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
 
 temp<-tempfile()
@@ -6,3 +9,14 @@ download.file(inputURL,temp)
 stormData <- read.csv(bzfile(temp))
 
 unlink(temp)
+
+
+head(stormData)
+
+str(stormData)
+
+# Across the United States, which types of events (as indicated in the EVTYPE 
+# variable) are most harmful with respect to population health?
+
+# Across the United States, which types of events have the greatest economic 
+# consequences?
